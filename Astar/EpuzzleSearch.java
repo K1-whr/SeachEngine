6c50d6678723a6246a2090316e9a0b3bf3a7bcc0
K1-whr <93322445+K1-whr@users.noreply.github.com>
2022-05-03 15:10:35 +0800

    Create EpuzzleSearch.java

---
+++ Astar/EpuzzleSearch.java
@@ -0,0 +1,18 @@
+
+/**
+ * @author KaiXin on 2022-05-01
+ * @version 1.8
+ * @since1.5
+ */
+public class EpuzzleSearch extends Search{
+
+    int[][] goal = new int[3][3];
+
+    public EpuzzleSearch(int[][] goal) {
+        this.goal = goal;
+    }
+
+    public String runSearch(EpuzzleState initState, String strat) {
+        return super.runSearch(initState, strat);
+    }
+}
